8e1a78cbd86074adb039ebebec2ea51f9f182768
Rafael Bomfim <58951965+rafaelbomfimjr@users.noreply.github.com>
2022-05-27 15:33:12 -0300

    Add files via upload

---
+++ restaurante/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from restaurante.models import Prato
+from restaurante.serializers import PratoSerializer
+
+class PratoListView(generics.ListAPIView):
+    queryset = Prato.objects.all()
+    serializer_class = PratoSerializer
